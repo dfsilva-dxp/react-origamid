82ed5193e94ec8130d32b3fcd1d7977fba589e0c
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-28 16:28:06 -0300

    feat: removendo os estilos do theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { ProductsContextProvider } from "./contexts/ProductsContext";
 import { AppRoutes } from "./routes/routes";
 
 import { GlobalStyle } from "./styles/global";
-import light from "./styles/themes/light";
 
 function App(): JSX.Element{
   return (
-    <ThemeProvider theme={light}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <AppRoutes />
-      </BrowserRouter>
-    </ThemeProvider>
+      <ProductsContextProvider>
+        <BrowserRouter>
+          <GlobalStyle />
+          <AppRoutes />
+        </BrowserRouter>
+      </ProductsContextProvider>
   );
 }
 
