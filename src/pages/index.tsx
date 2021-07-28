7350eafa8b7ebeab06723a697dc752c01823b76b
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-28 16:30:17 -0300

    feat: importando o component productList e passando a lista de produtos como props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React , { useEffect, useState }from "react";
 
 import Head from "../components/Head";
+import ProductList from "../components/ProductList";
+import useProduct from "../hooks/useProduct";
 import { Main } from "../styles/pages/home";
 
+type Photos = {
+  title: string;
+  src: string;
+}
+
+type Product = {
+  id: string;
+  fotos: Photos[];
+  nome: string;
+  preco: string;
+  descricao: string;
+  vendido: string;
+  usuario_id: string;
+}
+
 export function Home(): JSX.Element {
+  const [products, setProducts] = useState<Product[]>([])
+  const {getAllProducts} = useProduct();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getAllProducts();
+      setProducts([...response])
+    })()
+  }, [])
+
+
   return (
     <>
       <Head title="Home | app-commerce." description="Listagem de produtos do app-commerce."/>
       <Main>
         <div className="container">
-          <h1>Home</h1>
+          <ProductList list={products}/>
         </div>
       </Main>
     </>
