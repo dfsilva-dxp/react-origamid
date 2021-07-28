aed560a0fdc8bd8dd201e179e50db82e05ebf856
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-28 18:01:44 -0300

    feat: implementando a page contato

--- src/pages/contanto.tsx
+++ src/pages/contanto.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Head from "../components/Head";
 
+import contact_us from "../assets/contact_us.png"
+
 import * as S from "../styles/pages/contato";
 
 const Contanto = (): JSX.Element => {
@@ -9,7 +11,17 @@ const Contanto = (): JSX.Element => {
       <Head title="Contato | app-commerce." description="Listagem de produtos do app-commerce."/>
       <S.Main>
         <div className="container">
-          <h1>Contato</h1>
+          <S.Display className="fadeLeft">
+            <S.ImgCard style={{ background: `url(${contact_us}) no-repeat local 50% 50%`, backgroundSize: "cover"}} />
+            <S.ContactInfo>
+              <h1>Vamos conversar?</h1>
+              <ul>
+                <li>daniel@gmail.com</li>
+                <li>11 9 8380-4055</li>
+                <li>Av. Pres. Kenedy - Sesc</li>
+              </ul>
+            </S.ContactInfo>
+          </S.Display>
         </div>
       </S.Main>
     </>
--- src/styles/pages/contato.ts
+++ src/styles/pages/contato.ts
@@ -6,3 +6,27 @@ export const Main = styled.main`
   min-height: calc(100vh - 10rem);
   padding: 1.75rem 0;
 `
+
+export const Display = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  gap: 1rem;
+`
+
+export const ImgCard = styled.div`
+  max-width: 300px;
+  height: 450px;
+  border-radius: 8px;
+`
+
+export const ContactInfo = styled.div`
+  ul {
+    margin-top: 1rem;
+
+    li::before {
+      content: "---";
+      letter-spacing: -3px;
+      margin-right: 0.5rem;
+    }
+  }
+`
